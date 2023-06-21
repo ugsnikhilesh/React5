3b9fa2b15084f8574e1a11d38413a330cad3ff5f
ugsnikhilesh <gsnikhilesh.utukuri@gmail.com>
2023-06-21 09:45:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+  const [dateOfBirth, setDateOfBirth] = useState("");
+  const [age, setAge] = useState("");
+
+  const calculateAge = () => {
+    if (dateOfBirth) {
+      const birthDate = new Date(dateOfBirth);
+      const today = new Date();
+      const ageInMilliseconds = today - birthDate;
+      const ageInYears = Math.floor(
+        ageInMilliseconds / (1000 * 60 * 60 * 24 * 365)
+      );
+      setAge(ageInYears);
+    }
+  };
+
+  const handleDateChange = (event) => {
+    setDateOfBirth(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center border shadow p-4 m-5 mx-auto" style={{ width: '350px'}}>
+      <h1>Age Calculator</h1>
+      <div className="fs-5 m-2">Enter your date of birth:</div>
+      <div className="w-100">
+        <input className="p-2 m-2 w-75" style={{maxWidth: '300px'}} type="date" value={dateOfBirth} onChange={handleDateChange} />
+      </div>
+      <button className="btn btn-primary m-2" onClick={calculateAge}>Calculate Age</button>
+      {age>=0 && <div className="fs-4 fw-bold m-2">You are {age} years old!</div>}
+      {age<0 && <div className="fs-4 fw-bold m-2">Please give correct Date of Birth!</div>}
     </div>
   );
-}
+};
 
 export default App;
